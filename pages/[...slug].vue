f81cf87fbcbb9e42a87a0e740a5159a06e24eaa1
Misael Abanto <josemisaelabanto@gmail.com>
2023-08-04 21:44:24 -0500

    feat: content custom rendering

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const { path } = useRoute();
+const { data } = await useAsyncData('page-data', () =>
+	queryContent(path).findOne()
+);
+</script>
+
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+	<main>
+		<ContentRenderer v-if="data" :value="data">
+			<h1>{{ data.title }}</h1>
+			<ContentRendererMarkdown :value="data" />
+		</ContentRenderer>
+	</main>
 </template>
