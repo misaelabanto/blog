ed06a0c0fc15a8af362a5b7e78ef7c41336d2f04
Misael Abanto <josemisaelabanto@gmail.com>
2023-08-04 21:43:17 -0500

    feat: override content components

---
+++ components/content/ProseA.vue
@@ -0,0 +1,23 @@
+<template>
+	<NuxtLink
+		class="text-blue-500 decoration-none hover:decoration-solid"
+		:href="href"
+		:target="target"
+	>
+		<slot />
+	</NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	href: {
+		type: String,
+		default: '',
+	},
+	target: {
+		type: String,
+		default: undefined,
+		required: false,
+	},
+});
+</script>
---
+++ components/content/ProseCodeInline.vue
@@ -0,0 +1,5 @@
+<template>
+	<code class="font-mono text-orange-500">
+		<slot />
+	</code>
+</template>
