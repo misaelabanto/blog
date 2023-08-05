f0c41048b76731332bd46999be86cf549bc486c3
Misael Abanto <josemisaelabanto@gmail.com>
2023-08-04 21:44:34 -0500

    feat: index page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Skill } from '~/interfaces/Skill';
+const {
+	data: { value: skillsResponse },
+} = await useAsyncData('skills-data', () =>
+	queryContent<{ body: Skill[] }>('skills').findOne()
+);
+const skills = skillsResponse?.body;
+</script>
+
+<template>
+	<div>
+		hola
+		<ul>
+			<li v-for="skill in skills" :key="skill.name">
+				{{ skill.name }}: {{ skill.level }}
+			</li>
+		</ul>
+	</div>
+</template>
