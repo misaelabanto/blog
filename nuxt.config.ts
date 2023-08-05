55c11f26eab2171d6022cb8933a46329229d388c
Misael Abanto <josemisaelabanto@gmail.com>
2023-08-04 21:41:19 -0500

    feat: basic nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	modules: ['@nuxt/content', '@unocss/nuxt'],
+	app: {
+		head: {
+			title: 'Misael Blog',
+		},
+	},
+	components: [
+		{
+			path: '~/components',
+			global: true,
+		},
+	],
 });
