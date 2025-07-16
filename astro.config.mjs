751aa3dbf25e373f74ad135379ea3d05e5a26f1f
Misael Abanto <29669824+misaelabanto@users.noreply.github.com>
2025-07-16 08:12:22 -0500

    ✨ (astro.config.mjs): add internationalization support with default locale and locales array to enhance user experience ♻️ (Header.astro): refactor header component to remove unnecessary classes and add theme toggle functionality for improved UI 📝 (Header.astro): add script to manage theme preference in localStorage for persistent user experience ✨ (projects): update project technologies and descriptions to reflect recent changes and add new project "Buscar Placa" 🗑️ (data-pipeline-optimizer.json): remove deprecated project entry to keep project list relevant 🔧 (index.astro, BlogPost.astro): add theme management script to HTML for consistent theme application across pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'; // Import the official @tailwindcss/vite plugin
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://misaelabanto.com',
   integrations: [mdx(), sitemap()],
   output: 'static',
-
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+  },
   vite: {
      plugins: [tailwindcss()]
   }
