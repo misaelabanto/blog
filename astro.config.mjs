3a48b9d739a56d0e18518ad133b1c4c9b0bbf592
Misael Abanto <misaelabanto@gmail.com>
2025-09-04 07:16:12 +0200

    fix: add mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite'; // Import the official @tailwindcss/vite plugin
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://misaelabanto.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    mermaid({
+      autoTheme: true 
+    })
+  ],
   output: 'static',
   i18n: {
     defaultLocale: "en",
